c8a61ec71646a4cbbf9800cfc68f917825cf7f40
Idowu Emmanuel <idowuemmao@gmail.com>
2023-12-28 13:20:28 +0100

    updated the welcome image

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import welcome from "../images/welcome.jpg";
 const Home = () => {
   return (
     <div className="w-full h-full flex flex-col items-center mb-16">
-      <img src={welcome} alt="welcome" className="w-4/5 max-w-2xl" />
+      <img src={welcome} alt="welcome" className="w-3/5 max-w-2xl" />
       <div className=" w-9/12 flex gap-4">
         <a href="/sign-in" className="loginButton">
           Log in
